2400f027e85b3f963a825ab041b56ef7d8677db5
Nada-09 <nadaalmajidi4@gmail.com>
2024-09-26 11:51:34 +0300

    add icon

--- Water Tracker/ContentView.swift
+++ Water Tracker/ContentView.swift
@@ -10,10 +10,20 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Water Tracker 💦💦")
+            
+            HStack{
+                Text("Apple Health")
+                Toggle(isOn: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Is On@*/.constant(true)/*@END_MENU_TOKEN@*/) { }
+            }
+            
+            HStack{
+                Text("Cups to drink per day")
+                Stepper(value: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Value@*/.constant(4)/*@END_MENU_TOKEN@*/, in: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Range@*/1...10/*@END_MENU_TOKEN@*/) { }
+                }
+            
+            Button("Continue") { }
+            
         }
         .padding()
     }
