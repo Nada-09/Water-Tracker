3c69ad9dde00a6470f7099a958682e346adcad67
Nada-09 <nadaalmajidi4@gmail.com>
2024-09-26 11:22:32 +0300

    Initial Commit

---
+++ Water Tracker/Water_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Water_TrackerApp.swift
+//  Water Tracker
+//
+//  Created by Nada Abdullah on 23/03/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Water_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
